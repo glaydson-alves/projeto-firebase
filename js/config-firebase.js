de62281c25cae681257b3ae82e88ba3f3e249b56
Lucas Augusto <marquesribeirolucas@gmail.com>
2023-10-23 20:27:30 -0300

    criado projeto firebase

---
+++ js/config-firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyB2_ecNZCs3P-qG2ezBzu4TX8UJPfQJTIs",
+authDomain: "projeto-firebe-a72f9.firebaseapp.com",
+projectId: "projeto-firebe-a72f9",
+storageBucket: "projeto-firebe-a72f9.appspot.com",
+messagingSenderId: "579799090423",
+appId: "1:579799090423:web:f9f759fe4739923e8cfff2"
+};
+
+
+export const app = initializeApp(firebaseConfig);
